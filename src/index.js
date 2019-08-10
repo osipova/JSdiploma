5a2a03aa09a22841444d99e8411b934f89c4b052
Christina Osipova <chosipova@gmail.com>
2019-08-10 12:28:21 +0300

    questions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import popup from './modules/popup';
 import sentenceBtn from './modules/sentenceBtn';
+import questionsPanel from './modules/questionsPanel';
 
 popup();
 sentenceBtn();
+questionsPanel();
--- src/modules/questionsPanel.js
+++ src/modules/questionsPanel.js
@@ -1,15 +1,36 @@
 const questionsPanel = () => {
-  let answers = document.querySelectorAll('collapseThree-two');
-  let questions = document.querySelectorAll('headingOne-two');
+  let answers1 = document.getElementById('collapseOne-two'),
+    answers2 = document.getElementById('collapseTwo-two'),
+    answers3 = document.getElementById('collapseThree-two'),
+    questions = document.querySelector('.questions');
+
+  const inAnswers = (elem) => {
+    if (!elem.classList.contains('in')) {
+      elem.classList.add('in');
+    } else {
+      elem.classList.remove('in');
+    }
+  };
 
 
-  // questions.addEventListener('click', (event) => {
-  //   let target = event.target;
-  //   target.
-  //   for(let i = 0; i > answers.length; i++) {
-
-  //   }
-  // });
+  questions.addEventListener('click', (event) => {
+    console.log("here");
+    let target = event.target;
+    console.log(target);
+    if (target.closest('#headingOne-two')) {
+      inAnswers(answers1);
+      answers2.classList.remove('in');
+      answers3.classList.remove('in');
+    } else if (target.closest('#headingTwo-two')) {
+      inAnswers(answers2);
+      answers1.classList.remove('in');
+      answers3.classList.remove('in');
+    } else if (target.closest('#headingThree-two')) {
+      inAnswers(answers3);
+      answers2.classList.remove('in');
+      answers1.classList.remove('in');
+    }
+  });
 
 
 };
